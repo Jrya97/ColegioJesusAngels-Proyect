d9929ad67ec99d73bfeb7f3562d8ea635b012682
RenatoDev <renatoyaranga1997@gmail.com>
2025-10-09 15:38:51 -0500

    feat: eliminar componentes de header, carrusel de imágenes, inicio y logo

--- src/components/layout/logo.tsx
+++
@@ -1,9 +0,0 @@
-import logo from "../../assets/Logo-Jesus-Angels.jpg";
-
-export function Logo() {
-  return (
-    <div className="flex items-center gap-4">
-      <img className="w-70 h-30 object-scale-down" src={logo} alt="Logo Jesus Angels" />
-    </div>
-  );
-}
